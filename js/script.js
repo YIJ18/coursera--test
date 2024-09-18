f9b542cce3a1ca6b00adef383f6f0aa293777ac5
YIJ18 <jassoiris96@gmail.com>
2024-09-18 13:12:11 -0600

    javascript

--- js/script.js
+++ js/script.js
@@ -16,4 +16,33 @@ document.getElementById('formulario').addEventListener('submit', function(event)
     console.log("Tasa Anual: " + tasaAnual + "%");
     console.log("Pago Mensual: $" + pagoMensual);
 
+    const totalPagos = montoPrestamo * (1 + (tasaAnual / 100));
+    const interes = montoPrestamo*((tasaAnual/100)/12);
+    const capital = pagoMensual - interes;
+    const saldo = montoPrestamo - interes;
+    const numeroPago = 1;
+
+    //Genera una tabla con los valores
+    const tablaHTML = `
+        <h2>Evento</h2>
+        <table  class="table table-striped">
+            <tr>
+                <th> - </th>
+                <th>Monto de Préstamo</th>
+                <th>Interés</th>
+                <th>Capital</th>
+                <th>Saldo</th>
+            </tr>
+            <tr>
+                <td>${numeroPago}</td>
+                <td>$${montoPrestamo}</td>
+                <td>$${interes}</td>
+                <td>$${capital}</td>
+                <td>$${saldo}</td>
+            </tr>
+        </table>
+    `;
+
+    // Inserta la tabla en el div con id "tabla_resultado"
+    document.getElementById('tabla_resultado').innerHTML = tablaHTML;
 });
