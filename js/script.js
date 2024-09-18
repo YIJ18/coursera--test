4d188cb1f3083b2c259c81180ac05f289c2939dd
YIJ18 <jassoiris96@gmail.com>
2024-09-18 12:45:05 -0600

    Formulario javascript

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@ var company = new Object();
 company.name = "Facebook";
 company.ceo = "Mark";
 console.log(company);
+
+document.getElementById('formulario').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Valores de los campos
+    const montoPrestamo = document.getElementById('monto_prestamo').value;
+    const tasaAnual = document.getElementById('porcentaje').value;
+    const pagoMensual = document.getElementById('pago_mensual').value;
+
+    // Consola
+    console.log("Monto de Préstamo: $" + montoPrestamo);
+    console.log("Tasa Anual: " + tasaAnual + "%");
+    console.log("Pago Mensual: $" + pagoMensual);
+
+});
