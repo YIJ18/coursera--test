ff102df9b804e897f5bd91db5ae457e2213a370e
YIJ18 <jassoiris96@gmail.com>
2024-09-19 12:40:36 -0600

    terminado

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-document.getElementById('formulario').addEventListener('submit', function(event) {
+document.getElementById('formulario').addEventListener('submit', function (event) {
     event.preventDefault();
 
     // Valores de los campos
-    const montoPrestamo = document.getElementById('monto_prestamo').value;
-    const tasaAnual = document.getElementById('porcentaje').value;
-    const pagoMensual = document.getElementById('pago_mensual').value;
+    let montoPrestamo = parseFloat(document.getElementById('monto_prestamo').value);
+    const tasaAnual = parseFloat(document.getElementById('porcentaje').value);
+    const pagoMensual = parseFloat(document.getElementById('pago_mensual').value);
 
     // Consola
     console.log("Monto de Préstamo: $" + montoPrestamo);
@@ -14,13 +14,13 @@ document.getElementById('formulario').addEventListener('submit', function(event)
     //Variables que se usan para calcular los datos necesarios de la tabla
     let interes;
     let capital;
-    let saldo;
+    let saldo = montoPrestamo;
     let numeroPago = 1;
 
     //genera la base de la tabla
     let tablaHTML = `
-    <h2>Amortización</h2>
-    <table class="table table-striped">
+    <h2 class="text-center">Número de pagos aproximados</h2>
+    <table class="table table-striped text-center">
         <tr>
             <th># Pago</th>
             <th>Monto de Préstamo</th>
@@ -32,36 +32,26 @@ document.getElementById('formulario').addEventListener('submit', function(event)
 
     //Genera una tabla con los valores
 
-    do{
-
-        interes = montoPrestamo*((tasaAnual/100)/12);
+    do {
+        interes = saldo * ((tasaAnual / 100) / 12);
         capital = pagoMensual - interes;
         saldo = montoPrestamo - capital;
-
-        const tablaHTML = `
-        <h2>Evento</h2>
-        <table  class="table table-striped">
-            <tr>
-                <th> - </th>
-                <th>Monto de Préstamo</th>
-                <th>Interés</th>
-                <th>Capital</th>
-                <th>Saldo</th>
-            </tr>
+        
+        
+        tablaHTML += `
             <tr>
                 <td>${numeroPago}</td>
-                <td>$${montoPrestamo}</td>
-                <td>$${interes}</td>
-                <td>$${capital}</td>
-                <td>$${saldo}</td>
+                <td>$${montoPrestamo.toFixed(2)}</td>
+                <td>$${interes.toFixed(2)}</td>
+                <td>$${capital.toFixed(2)}</td>
+                <td>$${saldo.toFixed(2)}</td>
             </tr>
-        </table>
-    `;
-    montoPrestamo = saldo;
-    numeroPago++;
-    } while(Math.round(saldo) >= 0);
-    
-    
+        `;
+        montoPrestamo = saldo;
+        numeroPago++;
+    } while (Math.round(montoPrestamo) >= 0);
+
+    tablaHTML += '</table>';
 
     // Inserta la tabla en el div con id "tabla_resultado"
     document.getElementById('tabla_resultado').innerHTML = tablaHTML;
