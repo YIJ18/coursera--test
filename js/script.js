1f152b123c58be0af58f1f00deb5ef65bc84097e
YIJ18 <jassoiris96@gmail.com>
2024-09-20 12:56:01 -0600

    analisis financiero completo con validaciones

--- js/script.js
+++ js/script.js
@@ -12,14 +12,22 @@ document.getElementById('formulario').addEventListener('submit', function (event
     console.log("Pago Mensual: $" + pagoMensual);
 
     //Variables que se usan para calcular los datos necesarios de la tabla
+    var tabla = document.getElementById('tabla_resultado');
     let interes;
     let capital;
     let saldo = montoPrestamo;
     let numeroPago = 1;
+    let alerta;
+    let accion;
+    let tablaHTML;
 
-    //genera la base de la tabla
-    let tablaHTML = `
-    <h2 class="text-center">Número de pagos aproximados</h2>
+    tabla.innerHTML = '';
+
+    //Validacion para que no metan un monto de prestamo mayo al pago que haran mensualmente
+    if (montoPrestamo >= pagoMensual) {
+
+        tablaHTML =
+            `<h2 class="text-center">Número de pagos aproximados</h2>
     <table class="table table-striped text-center">
         <tr>
             <th># Pago</th>
@@ -27,32 +35,43 @@ document.getElementById('formulario').addEventListener('submit', function (event
             <th>Interés</th>
             <th>Capital</th>
             <th>Saldo</th>
-        </tr>
-`;
+        </tr>`;
 
-    //Genera una tabla con los valores
+        //Genera una tabla con los valores
+        do {
+            interes = saldo * ((tasaAnual / 100) / 12);
+            capital = pagoMensual - interes;
+            saldo = montoPrestamo - capital;
 
-    do {
-        interes = saldo * ((tasaAnual / 100) / 12);
-        capital = pagoMensual - interes;
-        saldo = montoPrestamo - capital;
-        
-        
-        tablaHTML += `
-            <tr>
+
+            accion = saldo > 0
+                ? 
+                tablaHTML+=`<tr>
                 <td>${numeroPago}</td>
                 <td>$${montoPrestamo.toFixed(2)}</td>
                 <td>$${interes.toFixed(2)}</td>
                 <td>$${capital.toFixed(2)}</td>
                 <td>$${saldo.toFixed(2)}</td>
-            </tr>
-        `;
-        montoPrestamo = saldo;
-        numeroPago++;
-    } while (Math.round(montoPrestamo) >= 0);
+            </tr>` :
+                alerta = `<div class="alert alert-success text-center" role="alert">
+                El ultimo pago, tan solo de $${ultimoPago.toFixed(2)}!
+            </div>`;
 
-    tablaHTML += '</table>';
+            montoPrestamo = saldo;
+            numeroPago++;
+            ultimoPago = saldo;
+        } while (montoPrestamo > 0);
 
-    // Inserta la tabla en el div con id "tabla_resultado"
-    document.getElementById('tabla_resultado').innerHTML = tablaHTML;
+        tablaHTML += '</table>';
+
+        // Inserta la tabla en el div con id "tabla_resultado"
+        document.getElementById('tabla_resultado').innerHTML = tablaHTML;
+    }
+    else {
+        alerta =  
+        `<div class="alert alert-danger text-center" role="alert">
+           No puedes ingresar un monto mayor  a un pago mensual!
+        </div>`;
+    }
+    document.getElementById('alerta').innerHTML = alerta;
 });
